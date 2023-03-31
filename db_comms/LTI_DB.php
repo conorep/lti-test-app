8070bfac1c1fd077e3ac2e1ed579054a35f7df48
Conor O'Brien <Conor.O'Brien@astronics.com>
2023-03-30 18:39:34 -0700

    moved out of unneeded src directory, made dummy DB into a class

---
+++ db_comms/LTI_DB.php
@@ -0,0 +1,13 @@
+<?php
+	namespace LTI_DB;
+	require_once __DIR__ . '/../vendor/autoload.php';
+	session_start();
+	use Packback\Lti1p3;
+	
+	/**
+	 * This placeholder class acts as a dummy database.
+	 */
+	class LTI_DB
+	{
+	
+	}
