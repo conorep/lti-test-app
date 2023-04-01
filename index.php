d0138df388aaf749bad7448a727cbfe60b5b9b45
Conor O'Brien <conorepobrien@gmail.com>
2023-03-31 23:35:35 -0700

    WELLP. I misread info - I do not need to try to break down the message sent by Canvas to verify a signature. I am only expected to send the lti_message_hint back to Canvas undisturbed so that Canvas can verify that against their own secret. Base64_decode with the first two portions of the JWT will get me the info I need for the next auth call in line - typ, alg, canvas_domain, context_type, context_id (client_id), exp. COOL.

--- index.php
+++ index.php
@@ -6,18 +6,12 @@
 	Firebase\JWT\JWT::$leeway = 5;
 	
 	/*hard coded, retrieved from queries*/
-	$jwtString = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjYW52YXNfZG9tYWluIjoiY29ub3JlcC1zdXByZW1lLWNhcHliYXJhLXI0NHZycXc5cmpoNnB4LTMwMDAucHJldmlldy5hcHAuZ2l0aHViLmRldi8iLCJjb250ZXh0X3R5cGUiOiJDb3Vyc2UiLCJjb250ZXh0X2lkIjoxMDAwMDAwMDAwMDAwMSwiY2FudmFzX2xvY2FsZSI6ImVuIiwiaW5jbHVkZV9zdG9yYWdlX3RhcmdldCI6dHJ1ZSwiZXhwIjoxNjgwMzA3OTQzfQ.edUe2H7h0N6DLuNA9uNTcVveck9Xzbsfm7V2qjYw7OY";
-	
-	$jwtsFromCanvas = file_get_contents('https://canvas.instructure.com/api/lti/security/jwks');
-	
-//	$jwtsFromCanvas = Firebase\JWT\JWT::urlsafeB64Decode($jwtsFromCanvas);
-	
-//	$jwtsFromCanvas = Firebase\JWT\JWT::jsonDecode($jwtsFromCanvas);
+	//$jwtString = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjYW52YXNfZG9tYWluIjoiY29ub3JlcC1zdXByZW1lLWNhcHliYXJhLXI0NHZycXc5cmpoNnB4LTMwMDAucHJldmlldy5hcHAuZ2l0aHViLmRldi8iLCJjb250ZXh0X3R5cGUiOiJDb3Vyc2UiLCJjb250ZXh0X2lkIjoxMDAwMDAwMDAwMDAwMSwiY2FudmFzX2xvY2FsZSI6ImVuIiwiaW5jbHVkZV9zdG9yYWdlX3RhcmdldCI6dHJ1ZSwiZXhwIjoxNjgwMzA3OTQzfQ.edUe2H7h0N6DLuNA9uNTcVveck9Xzbsfm7V2qjYw7OY';
+    $jwtString = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjYW52YXNfZG9tYWluIjoiY29ub3JlcC1zdXByZW1lLWNhcHliYXJhLXI0NHZycXc5cmpoNnB4LTMwMDAucHJldmlldy5hcHAuZ2l0aHViLmRldi8iLCJjb250ZXh0X3R5cGUiOiJDb3Vyc2UiLCJjb250ZXh0X2lkIjoxMDAwMDAwMDAwMDAwMSwiY2FudmFzX2xvY2FsZSI6ImVuIiwiaW5jbHVkZV9zdG9yYWdlX3RhcmdldCI6dHJ1ZSwiZXhwIjoxNjgwMzAwNDQ5fQ.0fqny8cKT1XaeevfC7K355vDClXnaxq7J75uxz-Ibe0';
 
-	$jwtsFromCanvas = json_decode($jwtsFromCanvas, true);
-	print_r($jwtsFromCanvas);
-	$publicKeys = Firebase\JWT\JWK::parseKeySet($jwtsFromCanvas);
-	
-	$decoded = Firebase\JWT\JWT::decode($jwtString, $publicKeys);
-	
-	var_dump($decoded);
+    $jwtArr = explode('.', $jwtString);
+
+    echo(base64_decode($jwtArr[0]));
+    echo(base64_decode($jwtArr[1]));
+    /*DON'T WORRY ABOUT THE SIG. THAT'S FOR CANVAS - SENDING IT BACK TO THEM ONLY.*/
+//    echo(base256_decode($jwtArr[2]));
