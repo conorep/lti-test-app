29199b830fdbb48530e5627955ae37db653f6565
Conor O'Brien <Conor.O'Brien@astronics.com>
2023-04-12 20:49:08 -0700

    moved logdata to function, toyed with curl to see if I could get codespace launch functional (couldn't), made a generate keys function

--- helper/helperfunctions.php
+++ helper/helperfunctions.php
@@ -13,7 +13,7 @@
          * @param string|null $path the path for setcookie
          * @return void
          */
-        public static function setGoodCookie(string $cookieName, string $cookieData, string|null $path = null): void
+        public static function setGoodCookie(string $cookieName, string $cookieData, ?string $path = null): void
         {
             $options =
                 [
@@ -50,7 +50,8 @@
          * @param $header array|null array of header info or null
          * @return bool|string|void
          */
-        public static function callAPI(string $method, string $url, array|null $data, array|null $header){
+        public static function callAPI(string $method, string $url, mixed $data, ?array $header){
+			$timeout = 10;
             $curl = curl_init();
             switch ($method){
                 case "POST":
@@ -70,7 +71,10 @@
 
             curl_setopt($curl, CURLOPT_URL, $url);
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($curl, CURLINFO_HEADER_OUT, 1);
+			curl_setopt($curl, CURLOPT_HEADER, 1);
             curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+			
             if($header != null)
             {
                 curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
@@ -81,9 +85,94 @@
             {
                 die("Connection Failure");
             }
+			
             curl_close($curl);
             return $result;
         }
+		
+		/**
+		 * This function creates a log file in the same location as the file that calls it.
+		 * It's currently setup to handle logging of objects, arrays, strings, integers, doubles, and booleans.
+		 * @param mixed $data
+		 * @throws UnexpectedValueException if data is null
+		 * @return void
+		 */
+		public static function logData(mixed $data): void
+		{
+			if($data == null)
+			{
+				throw new UnexpectedValueException("error - no logging null data!");
+			}
+			$logData = gettype($data) == 'object' || gettype($data) == 'array' ? print_r($data, true) : $data;
+			$log = "User: ".$_SERVER['REMOTE_ADDR'].' - '.date("F j, Y, g:i a").PHP_EOL.
+				"Data: ". $logData . ' ' . PHP_EOL .
+				'-------------------------' . PHP_EOL;
+			
+			file_put_contents('./log_'.date("j.n.Y").'.txt', $log, FILE_APPEND);
+		}
+		
+		/**
+		 * This function provides the means to generate a new OpenSSL asymmetric key pair (private and public) in the
+		 *    PEM format.
+		 * NOTE: The machine that is running these algorithms reportedly needs to have a valid openssl.cnf file
+		 *    installed on it.
+		 * NOTE: The key name can also be written in a way that provides a path to where you want the files saved.
+		 * @param string|null $privKeyName choose your own private key name/file type - default is priv.key
+		 * @param string|null $pubKeyName choose your own public key name/file type - default is pub.key
+		 * @param string|null $jwkName choose your own public JWK name/file type - default is pubkey.json
+		 * @param string|null $jwkID choose your own public JWK ID - default is uniqueWhaleSongGP2023
+		 * @return void
+		 */
+		public static function generateKeys(?string $privKeyName = 'priv.key', ?string $pubKeyName = 'pub.key',
+											?string $jwkName = 'pubkey.json', ?string $jwkID = 'uniqueWhaleSongGP2023'): void
+		{
+			// 1EdTech LTI specs require private key bits to be at least 2048. 4096 is being used here.
+			// 1EdTech also requires RSA key type
+			// RS256 is default, but the digest_alg option will be left here for information purposes (RS256 is another
+			// 	spec requirement)
+			$keyConfig = [
+				"digest_alg" => "RS256",
+				"private_key_bits" => 4096,
+				"private_key_type" => OPENSSL_KEYTYPE_RSA,
+			];
+			
+			// this generates a new openssl key
+			$privPem = openssl_pkey_new($keyConfig);
+			
+			// this exports  PEM format private key
+			openssl_pkey_export($privPem, $privKey);
+			
+			// this gets the public key for the private key
+			$pubPem = openssl_pkey_get_details($privPem)['key'];
+			
+			// this creates a private key file, given that there isn't already an existing file of that name in the
+			// 		directory. if there is, it will be overwritten.
+			openssl_pkey_export_to_file($privKey, $privKeyName);
+			
+			// the same idea applies to creating/overwriting a public key file
+			file_put_contents($pubKeyName, $pubPem);
+			
+			// get public key details for usage with public JWK generation
+			$keyInfo = openssl_pkey_get_details(openssl_pkey_get_public($pubPem));
+			
+			$jsonPub = [
+				'keys' => [
+					[
+						'kty' => 'RSA',
+						'n' => rtrim(str_replace(['+', '/'], ['-', '_'], base64_encode($keyInfo['rsa']['n'])), '='),
+						'e' => rtrim(str_replace(['+', '/'], ['-', '_'], base64_encode($keyInfo['rsa']['e'])), '='),
+						"alg" => "RS256",
+						"use" => "sig",
+						"kid" => "$jwkID"
+					]
+				]
+			];
+			
+			$pubJWK = json_encode($jsonPub, JSON_PRETTY_PRINT).PHP_EOL;
+			
+			//output your spiffy new public JWK
+			file_put_contents($jwkName, $pubJWK);
+		}
 
     /*end class*/
     }
